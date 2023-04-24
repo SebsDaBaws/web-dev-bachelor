05b72099a38b45642606ee5032b45294461373a3
Sebastian <101987260+SebsDaBaws@users.noreply.github.com>
2023-04-24 14:25:31 +0200

    Responsive header v1

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -4,35 +4,41 @@ import { JPLogo } from "../icons/jp_logo";
 export default component$(() => {
   return (
     <header>
-      <div class="mx-auto flex max-w-screen-xl items-center py-8 px-5 pt-16">
-        <div class="basis-64">
-          <time class="uppercase invisible md:visible">
+      <div class="mx-auto flex max-w-screen-xl items-center md:py-8 md:px-5 px-2 md:pt-16 pt-4">
+        <div class="basis-64 hidden md:block">
+          <time class="uppercase ">
             <span class="pr-1 font-bold">
               {new Date().toLocaleString("da-DK", { weekday: "long" })}
             </span>
             {new Date().toLocaleString("da-DK", { dateStyle: "long" })}
           </time>
         </div>
-        <div class="flex grow justify-center">
+        <div class="flex grow md:justify-center items-start">
           <a href="/">
             <JPLogo />
           </a>
         </div>
 
-        <ul class="flex flex-row flex-wrap text-right space-x-4 py-5 px-5 font-sans-serif font-medium text-sm">
-          <li class="border-r border-neutral-400 invisible md:visible">
+        <ul class="flex flex-row text-right md:space-x-2 space-x-2 md:py-5 md:px-5 font-sans-serif font-medium text-xs">
+          <li class="border-r border-neutral-400 hidden md:block">
             <button class="bg-transparent hover:underline text-black py-1 px-2 rounded uppercase tracking-widest">
               <a href="https://medielogin.dk/Jyllands-posten/login">Log ind</a>
             </button>
           </li>
 
           <li>
-            <button class="bg-transparent hover:bg-secondary text-secondary hover:text-white py-1 px-2 border border-secondary hover:border-transparent rounded uppercase tracking-widest">
+            <button
+              class="bg-transparent hover:bg-secondary text-secondary hover:text-white py-1 px-2 border 
+            border-secondary hover:border-transparent rounded uppercase tracking-widest"
+            >
               <a href="https://abonnement.jyllands-posten.dk/">Køb</a>
             </button>
           </li>
           <li>
-            <button class="bg-transparent hover:bg-primary text-black hover:text-white py-1 px-2 border border-black hover:border-transparent rounded uppercase tracking-widest">
+            <button
+              class="bg-transparent hover:bg-primary text-black hover:text-white py-1 px-2 border 
+            border-black hover:border-transparent rounded uppercase tracking-widest"
+            >
               <a href="/">Menu</a>
             </button>
           </li>
--- src/routes/pages/tag_page/index.tsx
+++ src/routes/pages/tag_page/index.tsx
@@ -1,15 +1,22 @@
-import { component$ } from '@builder.io/qwik';
-import { type DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import { type DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  
   return (
-    <div class="section">
-     
-    </div>
+    <article class="section">
+      <h1 class="justify-start flex uppercase px-3.5 font-semibold text-lg">
+        Emner du følger
+      </h1>
+
+      <div class="w-full invisible md:visible">
+        <hr class="h-1 my-12 bg-black border-0" />
+      </div>
+
+
+    </article>
   );
 });
 
 export const head: DocumentHead = {
-  title: '',
+  title: "",
 };
