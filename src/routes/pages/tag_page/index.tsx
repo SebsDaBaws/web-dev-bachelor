8def556593e569f65df931172ac3506d256ed170
Sebastian <101987260+SebsDaBaws@users.noreply.github.com>
2023-04-26 22:50:57 +0200

    Tag_page in progress (grids+mobile styling)

--- src/routes/pages/tag_page/index.tsx
+++ src/routes/pages/tag_page/index.tsx
@@ -4,61 +4,94 @@ import { type DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <div>
-      <h1 class="justify-start uppercase flex px-3.5 font-semibold text-lg hidden md:block">
+      <h1 class="justify-start uppercase flex px-6 font-semibold text-lg">
         Emner du følger
       </h1>
       <div class="w-full invisible md:visible">
-        <hr class="h-1 my-12 bg-black border-0" />
+        <hr class="h-1 my-8 md:my-12 bg-black border-0" />
       </div>
 
       <main class="md:grid md:grid-cols-12 md:divide-x md:divide-gray-400">
         <div class="md:col-span-3">
-          <div class="font-serif text-2xl font-semibold break-normal max-w-2xl px-3.5 justify-start">
+          {/* Desktop */}
+          <div class="font-serif text-2xl font-semibold break-normal max-w-2xl px-4 md:px-6 justify-start hidden md:block">
             Emner du følger
           </div>
-          <div class="px-3.5 justify-start">Seneste 48 timer</div>
-
-          
+          {/* Mobile */}
+          <div class="font-serif text-2xl font-bold break-normal max-w-2xl text-center px-4 md:px-6 justify-start block md:hidden">
+            Nyt fra emner du følger
+          </div>
+          <div class="px-4 md:px-6 justify-start text-center md:text-left pb-6 md:pb-0">
+            Seneste 48 timer
+          </div>
         </div>
 
         <ul class="flex flex-col md:flex-col md:divide-y justify-end md:col-span-9">
+          <div class="w-full block md:hidden">
+            <hr class="h-1 my-1 bg-gray-400 border-0" />
+          </div>
+
           <li class="pt-6 pb-4 mx-4">
-            {/* "id": 15279010 */}
+            {/* Article "id": 15279010 */}
             <a href="https://dev.jyllands-posten.dk/international/europa/ECE15279010/granater-fra-sovjettiden-regner-ned-over-bakhmut/">
-              <time>15:47</time>
-              <div class="text-primary">- Krigen i Ukraine</div>
+              <div class="pb-2 md:pb-1 text-xs">
+                <time>15:47 |</time>
+                <nobr class="text-primary font-semibold uppercase">
+                  {" "}
+                  Krigen i Ukraine{" "}
+                </nobr>
+              </div>
               <div class="flex flex-row justify-between border-r font-serif">
                 {" "}
                 Manglen på granater er ved at være mærkbar på begge sider i
                 Ukraine{" "}
               </div>
               <img
-                class="w-40 pt-2"
+                class="w-40 pt-2 self-end"
                 src="https://dev.billeder.jyllands-posten.dk/pictures/image/15044995/t3o5s8/ALTERNATES/master-free/artilleri-donbas"
               />
             </a>
           </li>
 
+          <div class="w-full block md:hidden">
+            <hr class="h-1 my-1 bg-gray-400 border-0" />
+          </div>
+
           <li class="py-4 mx-4">
-            {/* "id": 15316002 */}
-            <a href="https://dev.jyllands-posten.dk/politik/ECE15316002/eksperter-jon-stephensens-paastaaede-forseelse-er-ikke-grov-nok-til-en-politianmeldelse/">
-              <time>15:47 - Politik</time>
+            {/* Article "id": 15275559 */}
+            <a href="/">
+              <div class="pb-2 md:pb-1 text-xs">
+                <time>15:47 |</time>
+                <nobr class="text-primary font-semibold uppercase">
+                  {" "}
+                  Skole & uddannelse{" "}
+                </nobr>
+              </div>
               <div class="flex flex-row justify-between border-r font-serif">
                 {" "}
-                »Hvis Jon Stephensen havde trukket 5 mio. kr. ud, vil jeg tro,
-                at bestyrelsen var gået videre«{" "}
+                Forstandere taler om, at der er noget, forældrene har glemt{" "}
               </div>
               <img
                 class="w-40 pt-2"
-                src="https://dev.billeder.jyllands-posten.dk/pictures/image/15316021/mxx5t8/ALTERNATES/master-free/moderaternes-kulturordfoerer-jon-stephensen-moeder-pressen"
+                src="https://dev.billeder.jyllands-posten.dk/pictures/image/15313307/kfxrrl/ALTERNATES/master-free/efterskoler-kaemper-med-pyldrede-foraeldre"
               />
             </a>
           </li>
 
+          <div class="w-full block md:hidden">
+            <hr class="h-1 my-1 bg-gray-400 border-0" />
+          </div>
+
           <li class="py-4 mx-4">
-            {/* "id": 15273987 */}
+            {/* Article "id": 15273987 */}
             <a href="https://dev.jyllands-posten.dk/sport/fodbold/ECE15273987/lige-netop-dette-nederlag-gjorde-ekstra-ondt-for-kent-nielsen-og-co/">
-              <time>15:47 - Sport</time>
+              <div class="pb-2 md:pb-1 text-xs">
+                <time>15:47 |</time>
+                <nobr class="text-primary font-semibold uppercase">
+                  {" "}
+                  Sport{" "}
+                </nobr>
+              </div>
               <div class="flex flex-row justify-between border-r font-serif">
                 {" "}
                 Lige netop dette nederlag gjorde ekstra ondt for Kent Nielsen og
@@ -71,33 +104,45 @@ export default component$(() => {
             </a>
           </li>
 
+          <div class="w-full block md:hidden">
+            <hr class="h-1 my-1 bg-gray-400 border-0" />
+          </div>
+
           <li class="py-4 mx-4">
-            {/* "id": 15313540 */}
-            <a href="https://dev.jyllands-posten.dk/kultur/film/ECE15313540/danskere-i-verden-kunne-have-vaeret-et-mere-interessant-program-hvis-dr-havde-tilladt-sig-at-stille-sig-lidt-mere-kritisk/">
-              <time>15:47 - Kultur</time>
+            {/* Article "id": 15316002 */}
+            <a href="https://dev.jyllands-posten.dk/politik/ECE15316002/eksperter-jon-stephensens-paastaaede-forseelse-er-ikke-grov-nok-til-en-politianmeldelse/">
+              <div class="pb-2 md:pb-1 text-xs">
+                <time>15:47 |</time>
+                <nobr class="text-primary font-semibold uppercase">
+                  {" "}
+                  Politik{" "}
+                </nobr>
+              </div>
               <div class="flex flex-row justify-between border-r font-serif">
                 {" "}
-                DR presser igen igen fiktionaliserede elementer ned over
-                verdenshistorien{" "}
+                »Hvis Jon Stephensen havde trukket 5 mio. kr. ud, vil jeg tro,
+                at bestyrelsen var gået videre«{" "}
               </div>
               <img
                 class="w-40 pt-2"
-                src="https://dev.billeder.jyllands-posten.dk/pictures/image/15313543/1b5khn/ALTERNATES/master-free/15313540-danskere-i-verden-kunne-have-vaeret-et-mere-interessant-program-hvis-dr-havde-tilladt-sig-at-stille-sig-lidt-mere-kritisk"
+                src="https://dev.billeder.jyllands-posten.dk/pictures/image/15316021/mxx5t8/ALTERNATES/master-free/moderaternes-kulturordfoerer-jon-stephensen-moeder-pressen"
               />
             </a>
           </li>
 
-          <div class="px-4 items-center py-4">
-          <button
-            class="bg-transparent hover:bg-secondary text-secondary hover:text-white py-1.5 px-10 border 
-            border-secondary hover:border-transparent rounded uppercase text-xs font-semibold"
-          >
-            <a href="/">Indlæs flere artikler</a>
-          </button>
-        </div>
-        </ul>
+          <div class="w-full block md:hidden">
+            <hr class="h-1 my-1 bg-gray-400 border-0" />
+          </div>
 
-       
+          <div class="px-4 items-center py-4">
+            <button
+              class="bg-transparent hover:bg-secondary text-secondary hover:text-white py-1.5 px-10 border 
+            border-secondary hover:border-transparent rounded uppercase text-xs font-semibold"
+            >
+              <a href="">Indlæs flere artikler</a>
+            </button>
+          </div>
+        </ul>
       </main>
     </div>
   );
